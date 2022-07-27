d3592532001ef947a4852d35aaf61ecf3c5f27a7
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-07-26 20:57:33 -0600

    test environ

--- cat_collector/settings.py
+++ cat_collector/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import environ
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+env = environ.Env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v#nge!-v27dis6s^ai_^3&@g(t1qz8iijho70n*%0e6ubz9*=i'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
